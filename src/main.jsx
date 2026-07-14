0d8b01c3db871222a2d3fc0e6e0f41b720cd030f
Pallavi <shivannagowdahk@gmail.com>
2026-07-14 09:49:34 +0530

    fix: inject basename routing parameters for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import About from './pages/About.jsx'
 import Projects from './pages/Projects.jsx'
-import Contact from './pages/Contact.jsx' // Imported new page
+import Contact from './pages/Contact.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* Adding the basename property lets React Router know it lives in a subfolder */}
+    <BrowserRouter basename="/Portfolio">
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<About />} />
           <Route path="projects" element={<Projects />} />
-          <Route path="contact" element={<Contact />} /> {/* Added route */}
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
